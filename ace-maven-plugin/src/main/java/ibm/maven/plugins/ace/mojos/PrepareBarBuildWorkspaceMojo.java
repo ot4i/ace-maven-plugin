b488977919c4964b9da89c6ccc38d4364b9a8afb
ChrissWeiss <chrweiss@gmx.de>
2022-07-04 10:11:54 +0200

    added handling for custom maven settings file

--- ace-maven-plugin/src/main/java/ibm/maven/plugins/ace/mojos/PrepareBarBuildWorkspaceMojo.java
+++ ace-maven-plugin/src/main/java/ibm/maven/plugins/ace/mojos/PrepareBarBuildWorkspaceMojo.java
@@ -14,10 +14,13 @@ import ibm.maven.plugins.ace.utils.PomXmlUtils;
 import net.lingala.zip4j.ZipFile;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -45,7 +48,8 @@ import org.codehaus.plexus.util.FileUtils;
 import ibm.maven.plugins.ace.generated.maven_pom.Model;
 
 /**
- * Unpacks the dependent WebSphere Message Broker Projects.
+ * Two tasks: - unpacks any project dependencies - writes
+ * org.eclipse.m2e.core.prefs if "custom maven settings.xml" should be used
  * 
  * Implemented with help from:
  * https://github.com/TimMoore/mojo-executor/blob/master/README.md
@@ -54,6 +58,7 @@ import ibm.maven.plugins.ace.generated.maven_pom.Model;
  * goal to work correctly. See
  * https://github.com/TimMoore/mojo-executor/issues/3
  */
+
 @Mojo(name = "prepare-bar-build-workspace", requiresDependencyResolution = ResolutionScope.TEST)
 public class PrepareBarBuildWorkspaceMojo extends AbstractMojo {
 
@@ -61,7 +66,6 @@ public class PrepareBarBuildWorkspaceMojo extends AbstractMojo {
 	 * a comma separated list of dependency types to be unpacked
 	 */
 	private static final String UNPACK_ace_DEPENDENCY_TYPES = "zip";
-
 	private static final String UNPACK_ace_DEPENDENCY_SCOPE = "compile";
 
 	/**
@@ -94,33 +98,26 @@ public class PrepareBarBuildWorkspaceMojo extends AbstractMojo {
 	@Parameter(property = "ace.unpackDependenciesDirectory", defaultValue = "${project.basedir}/../dependencies/${project.artifact.artifactId}", required = true, readonly = true)
 	protected File unpackDependenciesDirectory;
 
-
+	/**
+	 * custom maven settings.xml
+	 */
+	@Parameter(property = "ace.customMavenSettings", defaultValue = "")
+	protected String customMavenSettings;
 
 	public void execute() throws MojoExecutionException, MojoFailureException {
 
 		unpackaceDependencies();
 
-	}
-
-	/**
-	 * @param pomFile
-	 * @return dummy comment
-	 */
-	private boolean isJarPackaging(File pomFile) {
 		try {
-			Model model = PomXmlUtils.unmarshallPomFile(pomFile);
-
-			// packaging "jar" is the default and may not be defined
-			if (model.getPackaging() == null || model.getPackaging().equals("") || model.getPackaging().equals("jar")) {
-				return true;
-			}
-		} catch (JAXBException e) {
-			getLog().debug("Exception unmarshalling ('" + pomFile.getAbsolutePath() + "')", e);
+			handleCustomMavenSettings();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
-		// this should really never happen
-		return false;
 	}
+
+	
 
 	/**
 	 * goal of the method is to create sharedLibs projects defined by Maven
@@ -138,8 +135,9 @@ public class PrepareBarBuildWorkspaceMojo extends AbstractMojo {
 		// define the directory to be unpacked into and create it
 		workspace.mkdirs();
 
-		// step 1: 
-		// unpack all dependencies that match the given scope; target: unpackDependencyDirectory 
+		// step 1:
+		// unpack all dependencies that match the given scope; target:
+		// unpackDependencyDirectory
 		executeMojo(plugin(groupId("org.apache.maven.plugins"), artifactId("maven-dependency-plugin"), version("2.8")),
 				goal("unpack-dependencies"),
 				configuration(element(name("outputDirectory"), unpackDependenciesDirectory.getAbsolutePath()),
@@ -148,73 +146,119 @@ public class PrepareBarBuildWorkspaceMojo extends AbstractMojo {
 				executionEnvironment(project, session, buildPluginManager));
 
 		try {
-		
+
 			// step 2: unpack all source files
 			if (unpackDependenciesDirectory.exists()) {
 				List<File> sourceFiles = FileUtils.getFiles(unpackDependenciesDirectory, "*sources.jar", "default.jar");
 				for (File sourceFile : sourceFiles) {
-					String[] fileParts =  (FileUtils.removeExtension(sourceFile.getName())).split("-");
-					String projectName = fileParts[0]; 
-					getLog().info("found source for project: "+projectName); 
-					
-					//define target environment and unpack sources 
+					String[] fileParts = (FileUtils.removeExtension(sourceFile.getName())).split("-");
+					String projectName = fileParts[0];
+					getLog().info("found source for project: " + projectName);
+
+					// define target environment and unpack sources
 					String targetDirectory = workspace.getAbsolutePath().toString() + "/" + projectName;
 					File projectDirectory = new File(targetDirectory);
-					
-					
+
 					new ZipFile(sourceFile).extractAll(projectDirectory.getAbsolutePath());
 					getLog().info("unpacking " + sourceFile.getName() + " to "
 							+ projectDirectory.getAbsolutePath().toString());
-					/*
-					//patch .project file 
-					//TODO: current assumption that we only handle "shared libs"  
-					ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-					InputStream inStream = classloader.getResourceAsStream("templates/project.txt");
-
-					String projectFile = IOUtils.toString(inStream);
-					projectFile = projectFile.replace("projectname", projectName);
-
-					String targetFileName = workspace.getAbsolutePath().toString() + "/" + projectName + "/.project";
-					File targetFile = new File(targetFileName);
-
-					Files.write(Paths.get(targetFile.getAbsolutePath()), projectFile.getBytes(StandardCharsets.UTF_8));
-						*/			}
+				}
 			} else {
 				getLog().info("unpack dependency directory does not exist");
 			}
-			/* 	
-			// step 3: unpack all sharedlibs - and unpack them directly to the workspace
-			if (unpackBarDirectory.exists()) {
-				List<File> sharedLibs = FileUtils.getFiles(unpackBarDirectory, "*.shlibzip", "default.shlibzip");
-				for (File sharedLib : sharedLibs) {
-					String projectName = FileUtils.removeExtension(sharedLib.getName());
-
-					// determine the targetDirectory
-					String targetDirectory = workspace.getAbsolutePath().toString() + "/" + projectName;
-					File projectDirectory = new File(targetDirectory);
-
-					new ZipFile(sharedLib).extractAll(projectDirectory.getAbsolutePath());
-					getLog().info("unpacking " + sharedLib.getName() + " to "
-							+ projectDirectory.getAbsolutePath().toString());
-
-					ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-					InputStream inStream = classloader.getResourceAsStream("templates/project.txt");
-
-					String projectFile = IOUtils.toString(inStream);
-					projectFile = projectFile.replace("projectname", projectName);
-
-					String targetFileName = workspace.getAbsolutePath().toString() + "/" + projectName + "/.project";
-					File targetFile = new File(targetFileName);
-
-					Files.write(Paths.get(targetFile.getAbsolutePath()), projectFile.getBytes(StandardCharsets.UTF_8));
-
-				}
-			} else {
-				getLog().info("unpack bar directory does not exist");
-			}
-		*/ 
+			/*
+			 * // step 3: unpack all sharedlibs - and unpack them directly to the workspace
+			 * if (unpackBarDirectory.exists()) { List<File> sharedLibs =
+			 * FileUtils.getFiles(unpackBarDirectory, "*.shlibzip", "default.shlibzip"); for
+			 * (File sharedLib : sharedLibs) { String projectName =
+			 * FileUtils.removeExtension(sharedLib.getName());
+			 * 
+			 * // determine the targetDirectory String targetDirectory =
+			 * workspace.getAbsolutePath().toString() + "/" + projectName; File
+			 * projectDirectory = new File(targetDirectory);
+			 * 
+			 * new ZipFile(sharedLib).extractAll(projectDirectory.getAbsolutePath());
+			 * getLog().info("unpacking " + sharedLib.getName() + " to " +
+			 * projectDirectory.getAbsolutePath().toString());
+			 * 
+			 * ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+			 * InputStream inStream =
+			 * classloader.getResourceAsStream("templates/project.txt");
+			 * 
+			 * String projectFile = IOUtils.toString(inStream); projectFile =
+			 * projectFile.replace("projectname", projectName);
+			 * 
+			 * String targetFileName = workspace.getAbsolutePath().toString() + "/" +
+			 * projectName + "/.project"; File targetFile = new File(targetFileName);
+			 * 
+			 * Files.write(Paths.get(targetFile.getAbsolutePath()),
+			 * projectFile.getBytes(StandardCharsets.UTF_8));
+			 * 
+			 * } } else { getLog().info("unpack bar directory does not exist"); }
+			 */
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * goal of the method is create the org.eclipse.m2e.core.prefs file - in the
+	 * case that a "custom maven settings.xml file" is set this is required as
+	 * mqsicreatebar - using Eclipse under the cover - is not aware of theses
+	 * settings
+	 * 
+	 * @throws MojoExecutionException If an exception occurs
+	 * @throws IOException
+	 */
+	private void handleCustomMavenSettings() throws MojoExecutionException, IOException {
+
+		if ((customMavenSettings!=null) && (!(customMavenSettings.equalsIgnoreCase("")))) {
+
+			getLog().info("create org.eclipse.m2e.core.prefs for custom maven settings: " + customMavenSettings);
+			// customMavenSettings is set
+			String targetDirectory = workspace.getAbsolutePath().toString()
+					+ "/.metadata/.plugins/org.eclipse.core.runtime/.settings";
+			String targetFileName = targetDirectory + "/org.eclipse.m2e.core.prefs";
+
+			//note this would work as well 
+			//however not working with templates/org.eclipse.m2e.core2.prefs
+			// ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+			//InputStream inStream4 = classloader.getResourceAsStream("templates/project.txt");
+
+			
+			File fdir = new File(targetDirectory);
+			File fout = new File (targetFileName);
+			getLog().info("creating new prefs file directory structure:"+ fdir.mkdirs());
+			
+			
+		 
+			getLog().info("creating new prefs file:"+ fout.createNewFile());
+			getLog().info("start writing to file ...");
+			
+			FileOutputStream fos = new FileOutputStream(fout);
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+			
+			bw.write("eclipse.m2.defaultRuntime=EMBEDDED"); 
+			bw.newLine();
+			bw.write("eclipse.m2.runtimes=");
+			bw.newLine();
+			bw.write("eclipse.m2.userSettingsFile="+customMavenSettings); 
+			bw.newLine();
+			bw.write("eclipse.preferences.version=1");
+			bw.close();
+			
+			
+			/* 
+			projectFile = projectFile.replace("settingsPath", customMavenSettings);
+
+			File targetFile = new File(targetFileName);
+
+			Files.write(Paths.get(targetFile.getAbsolutePath()), projectFile.getBytes(StandardCharsets.UTF_8));
+			*/
+
+		} else {
+			getLog().info("using standard maven settings"); 
 		}
 
 	}
